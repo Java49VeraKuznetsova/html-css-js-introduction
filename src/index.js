48524b8e7b388d34edafa0b88dc32183367add81
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-10-24 21:31:21 +0300

    HW10 - animation

--- src/index.js
+++ src/index.js
@@ -2,8 +2,12 @@ console.log ("Only for test - all good from the first time")
 const anchors = document.querySelectorAll(".thumbnail-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
+
 const mainClass = document.querySelector(".main-class");
-const HIDDEN = "hidden"
+const detailsContainer = document.querySelector(".details-container");
+const HIDDEN = "hidden";
+const IS_POINT ="is-point";
+
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
@@ -15,12 +19,19 @@ for (let i=0; i<anchors.length; i++) {
     })
 }
 
-function showDetails () {
+function showDetails() {
     /* delete Class Hidden - return Details on Screen */
     mainClass.classList.remove(HIDDEN);
+      /* decrease Det */
+    detailsContainer.classList.add(IS_POINT);
+     /* 1 ms - and begin increase */
+    setTimeout(function() {
+        detailsContainer.classList.remove(IS_POINT);
+    },1)
 }
 
 function hideDetails () {
      /* delete Details from Screen - return Class Hidden */
     mainClass.classList.add(HIDDEN);
+    
 }

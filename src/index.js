ea1901b5b6219f175c71bd869fd0cdd4a2d6cf8c
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-10-25 15:25:36 +0300

    HW10: animation and audio (full time det.image)

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ console.log ("Only for test - all good from the first time")
 const anchors = document.querySelectorAll(".thumbnail-anchor");
 const detailsImage = document.querySelector(".details-image");
 const detailsTitle = document.querySelector(".details-title");
-
+const detailsAudio = document.querySelector(".details-audio");
 const mainClass = document.querySelector(".main-class");
 const detailsContainer = document.querySelector(".details-container");
 const HIDDEN = "hidden";
@@ -11,12 +11,23 @@ const IS_POINT ="is-point";
 function setDetails(anchor) {
     detailsImage.setAttribute('src', anchor.getAttribute('data-details-image'));
     detailsTitle.innerHTML = anchor.getAttribute('data-details-title');
+    detailsAudio.setAttribute('src', anchor.getAttribute('data-details-audio'));
+     detailsAudio.play(); 
+      /* play for all the time, when det-image */
+    detailsAudio.loop=true;
+   
+   /* stop Audio after 10 sec - time of change image size*/
+ /* setTimeout(function() {
+    detailsAudio.pause();
+ },10000) */
 }
+
+
 for (let i=0; i<anchors.length; i++) {
     anchors[i].addEventListener("click", function() {
         showDetails ();
         setDetails(anchors[i]);
-    })
+           })
 }
 
 function showDetails() {
@@ -28,10 +39,14 @@ function showDetails() {
     setTimeout(function() {
         detailsContainer.classList.remove(IS_POINT);
     },1)
+    
 }
 
 function hideDetails () {
      /* delete Details from Screen - return Class Hidden */
     mainClass.classList.add(HIDDEN);
+    /* when click Button Close - stop musik */
+    /* delete if musik only for time of change size of Det-image */
+    detailsAudio.pause();
     
 }

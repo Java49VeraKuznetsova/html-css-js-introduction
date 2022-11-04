5f6a094cfab58f523e63e8b0f7bbe80557a43150
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-11-04 22:23:07 +0200

    HW13  - two tasks: check and created Teudat Zehut

--- app.js
+++ app.js
@@ -1,187 +1,122 @@
-console.log ("Homework 12");
+console.log ("Homework 13");
 
-/* !!! validation for numbers, base, string and negative numbers! */
+let teudatStrNumber = '345714521';
+console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber), '\n');
 
-let string1 = 'java', string2 = 'react', string3 = '10110011011011';
-let base1 = 36, base2 = 36, base3 = 2;
-let number1 = 900550, number2 = 46016237, number3 = 11483;
+teudatStrNumber = '345714520';
+console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber), '\n');
 
-function FromStringToNumber(string, base)
+teudatStrNumber = '34571452N';
+console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber), '\n');
+
+teudatStrNumber = '34571452';
+console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber),'\n');
+
+teudatStrNumber = '324505122';
+console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber),'\n');
+
+
+console.log ("Create TD:", generateRandomTeudatZehut());
+
+// first task
+function checkTeudatZehut(teudatStrNumber) 
 {
-   let result = 0; 
-  
-  for (let i = 0; i < string.length; i++)
-   {
-      let code =  string.charCodeAt(i); /* sumbol --> ACKII */
-  if (code >= 48 && code <= 57) /* ASKII -> 10, numbers */
-  {
-    code -= 48;
-  }
-  if (code >=97 && code <= 122) /* ASKII -> 10, letters */
-  {
-    code -=87;
-  }
-   result = result*base + code; /* turn number in base to 10-number */
-  }
-  return result;
+   let arNumberTZ = StringToNumber (teudatStrNumber); //TZ - array of nimbers
+   let check = checkInput (arNumberTZ); //check input of TZ: lenght, symbols
+
+// if input - O.K., check control sum
+  if (check == true)  
+    { 
+      check = false;  
+         let sumTD =  arNumberTZ.reduce (sumTZ,0);
+         if (sumTD % 10 == 0)
+             check = true;
+         else  
+             console.log ("Error3: incorrect TZnumber");
+    }           
+  return check;
 }
 
-function fromNumberToString (number, base)
+function StringToNumber (teudatStrNumber) //function to arrange string to array of numbers
 {
-  number = Math.abs(number);
-   let result = '';
-      
-   do{
-    let digit = number % base;
-    if (digit >= 0 && digit <= 9) /* 10 -> ASKII, numbers */
-    {
-      digit += 48;
-    }
-    if (digit >=10 && digit <= 35) /* 10 -> ASKII, letters */
-    {
-      digit +=87;
-    }
-    char = String.fromCharCode(digit); /*turn ACCII to symbol */
-    result = char + result; /* !! += - reverse order - error */
-    number = Math.floor (number/base);
-   
-   } while (number != 0);
-   return result;
+  let arSymbolTZ = Array.from(teudatStrNumber); /*array of symbols: string */
+  let arNumberTZ =  arSymbolTZ.map (symToNumber); /* function get array of numbers */
+     return arNumberTZ;
 }
 
-
-console.log(string1, '=', FromStringToNumber(string1, base1));
-console.log(string2, '=', FromStringToNumber(string2, base2)); 
-console.log(string3, '=', FromStringToNumber(string3, base3)); 
-
-console.log(number1,'=', fromNumberToString(number1, base1));
-console.log(number2,'=', fromNumberToString(number2, base2)); 
-console.log(number3,'=', fromNumberToString(number3, base3)); 
-
-/* console.log("Hello world"); */
-// for( i = 0; i < 3; i++) {
-//     setTimeout(function(){
-//         console.log(i)
-//     });
-// }
- // i = 100;
-// for (let i = 0; i < 3; i++) {
-//     setTimeout(function () { console.log(i) });
-// } => 0 1 2
-
-// for (var i = 0; i < 3; i++) {
-//      console.log(i) ;
-// }
-// function sum(op1, op2) {
-//     let res = op1 + op2;
-//     return res;
-// }
-// let op1 = 10;
-// let op2 = 20;
-// let res = sum(op1, op2);
-// console.log(res)
-/************************************************** */
-//HW #11
-// function sumDigits(number) {
-//     //compute sum of digits for the integer numbers
-//    number = Math.abs(number);
-//    let sum = 0;
-//    number = Math.trunc(number); //get rid of the fractional part
-//    do {
-//         let digit = number % 10;
-//         number = (number - digit) / 10;
-//         sum += digit;
-//    }while(number != 0);
-//   return sum;
-
-// }
-// //Example
-// let sum = sumDigits(123)
-// console.log(sum); //should be printed out 6
-
-
-/*********************************CW 12 */
-// let strNum1 = "123";
-// let strNum2 = '9';
-// console.log(strNum1 + strNum2); //=> 1239
-// console.log(strNum1 - strNum2); //=> 114
-// console.log(strNum1 > strNum2);
-//conversions from string to number
-// let num1 = +strNum1;
-// let num2 = +strNum2;
-// console.log(num1 + num2); //=> 132
-// console.log(num1 - num2); //=> 114
-// console.log(num1 > num2); //true
-// let strNumStr = '12.3ab';
-// let numStr = +strNumStr;
-// console.log(numStr);
-// let num = parseInt(strNumStr);
-// if (isNaN(numStr)) {
-//     console.log("numStr is NaN")
-// }
-// let num10 = 123;
-// let strNum10 = "" + num10;
-// let strNum16 = num10.toString(36);
-// console.log(strNum10, strNum16);
-
-//conversion from Morse code to number
-//symbol "." -> 0 ; "-" -> 1
-// function  fromMorseToNumber(morseCode) {
-//     let result = 0;
-//     for (let i = 0; i < morseCode.length; i++) {
-//         let code = morseCode[i] == '.' ? 0 : 1;
-//         result = result * 2 + code;
-//     }
-//     return result;
-// }
-// console.log(fromMorseToNumber('-.-...-.---.-.-..--'));
-// function fromNumberToMorse(number) {
-//     number = Math.abs(number);
-//     let res = "";
-//     do {
-//         let digit = number % 2;
-//         let sym = digit == 0 ? '.' : '-';
-//         res = sym + res;
-//         number = Math.trunc(number / 2);
-
-//     } while(number != 0);
-//     return res;
-// }
-// console.log(fromNumberToMorse(333651));
-
-/* Code from Yuri */
-
-function getSymbol(digit) {
-    let codeA = 'a'.charCodeAt();
-    if (digit > 9) {
-        digit = String.fromCharCode(codeA + digit - 10);
-    }
-      
-   return digit;
-    //31 - 10 + 97
-}
-function fromNumberToString(number, base) {
-    number = Math.abs(number);
-    let res = "";
-    do {
-        let digit = number % base;
-        let sym = getSymbol(digit);
-        res = sym + res;
-        number = Math.trunc(number / base);
-  
-
+function symToNumber (symbol, index) //function to arrange symbol to number
+{
  
-    string = string.toLowerCase();
-    let result = 0;
-    for (let i = 0; i < string.length; i++) {
-        let digit = getDigit(string[i]);
-        result = result * base + digit;
-    }
-    return result;
+  return Number(symbol);
 } 
 
-//getting code from symbol 
-//console.log('abc'.charCodeAt(2))
+function checkInput (arNumberTZ) //function for checking input
+{
+    let check = false;
+    if (arNumberTZ.length == 9)
+     {
+      arNumberTZ.forEach(function(symbol)
+      { 
+        if (symbol >= 0 && symbol <= 9)
+            { check = true;}
+        else
+            {console.log ("Error1: not a number");
+              check = false;  
+           } 
+      });
+     }
+    else {
+      console.log ("Error2: incorrest length");
+        }
+  return check;
+}
 
+function sumTZ (total, symbol, index) //function to calc. control sum
+{
+  if (index % 2 == 0)
+     { total += symbol; 
+     }
+  else {
+    let oddN = symbol*2;
+    if (oddN >9)
+      { total += (oddN % 10) + 1; 
+      }     
+    else  {
+      total += oddN;
+          }    
+  }
+  return total;  
+}
 
+// second task
+function generateRandomTeudatZehut()
+{
+  // create array from 8 elements
+  let arTZnumber = genArray8();
+  
+  // put control sum to 9-element
+  arTZnumber[8] =  10 - (arTZnumber.reduce (sumTZ,0) % 10);
+  // change array from number to symbol
+  let arSymbolTZ =  arTZnumber.map (numberToSym); 
+  // get string from array of symbols without spasee and comma
+  let newTZ = arSymbolTZ.join('');
+   // check new TZ
+  console.log ("New TD: ", newTZ, "Answer: ", checkTeudatZehut(newTZ),'\n');
 
+  return newTZ;
+} 
+
+function genArray8 () //function to get random array [8]
+{
+  let arNumberTZ8 = [];
+  for (let i=0; i < 8; i++) 
+     {
+      arNumberTZ8[i] = Math.round(Math.random() * 9);
+     }
+   return arNumberTZ8;
+}
+
+function numberToSym (symbol) //function to arrange number to string
+{
+   return String(symbol);
+} 

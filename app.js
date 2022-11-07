be1b403897ba65fe41fcb227465593962f8dc163
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-11-07 18:17:48 +0200

    HW13 + HW13 in classwork 14

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZe
 teudatStrNumber = '345714520';
 console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber), '\n');
 
-teudatStrNumber = '34571452N';
+teudatStrNumber = '3457N4523';
 console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber), '\n');
 
 teudatStrNumber = '34571452';
@@ -37,6 +37,9 @@ function checkTeudatZehut(teudatStrNumber)
   return check;
 }
 
+/* V.R. May be following is easier.
+  return Array.from(teudatStrNumber).map(symToNumber);
+  */
 function StringToNumber (teudatStrNumber) //function to arrange string to array of numbers
 {
   let arSymbolTZ = Array.from(teudatStrNumber); /*array of symbols: string */
@@ -46,24 +49,35 @@ function StringToNumber (teudatStrNumber) //function to arrange string to array 
 
 function symToNumber (symbol, index) //function to arrange symbol to number
 {
- 
+   /* V.R. May be
+  return +symbol;
+  is easier
+  */
   return Number(symbol);
 } 
 
 function checkInput (arNumberTZ) //function for checking input
 {
-    let check = false;
-    if (arNumberTZ.length == 9)
+    let check = true; 
+   if (arNumberTZ.length == 9)
      {
+      /* !!!!! V.R. This cycle gives true if the LAST number is OK.
+      It isn't correct. It is necessary to fix.
+      */
+     
       arNumberTZ.forEach(function(symbol)
       { 
         if (symbol >= 0 && symbol <= 9)
             { check = true;}
         else
+        /* V.R. The function like this hasn't to log.
+        It returns boolean value and caller logs.
+        */
             {console.log ("Error1: not a number");
               check = false;  
-           } 
+            } 
       });
+     
      }
     else {
       console.log ("Error2: incorrest length");
@@ -79,8 +93,10 @@ function sumTZ (total, symbol, index) //function to calc. control sum
   else {
     let oddN = symbol*2;
     if (oddN >9)
-      { total += (oddN % 10) + 1; 
-      }     
+      { 
+        total += (oddN % 10) + 1; 
+        // V.R. total += (oddN % 10) + Math.floor(oddN/10);
+      }       
     else  {
       total += oddN;
           }    
@@ -95,7 +111,16 @@ function generateRandomTeudatZehut()
   let arTZnumber = genArray8();
   
   // put control sum to 9-element
-  arTZnumber[8] =  10 - (arTZnumber.reduce (sumTZ,0) % 10);
+
+  //!!! for case if control sum is multiplied on 10
+  // you get digit 10 
+  //that can not be in the regular decimal computing system
+  //arTZnumber[8] =  10 - (arTZnumber.reduce (sumTZ,0) % 10);
+  // this is new code:
+  let controlNumber = arTZnumber.reduce (sumTZ,0) % 10;
+  controlNumber == 0 ? arTZnumber[8] = controlNumber : arTZnumber[8] = 10-controlNumber;
+   
+       
   // change array from number to symbol
   let arSymbolTZ =  arTZnumber.map (numberToSym); 
   // get string from array of symbols without spasee and comma
@@ -118,5 +143,7 @@ function genArray8 () //function to get random array [8]
 
 function numberToSym (symbol) //function to arrange number to string
 {
+  // V.R. String.fromCharCode(cur+'0'.charCodeAt());
    return String(symbol);
 } 
+

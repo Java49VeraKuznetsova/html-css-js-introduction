ea2fa7964e60e627b52e9d1e38e2ba015adc5bc2
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-11-08 14:57:12 +0200

    HW14 - array methods, 4 tasks (rename files)

--- app.js
+++ app.js
@@ -1,149 +1,85 @@
-console.log ("Homework 13");
+console.log ("Homework 14");
 
-let teudatStrNumber = '345714521';
-console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber), '\n');
+let strings = ["abc", "lmn", "cd","abc","abc"];
+let numbers = [1, 2, 3, 4, 5, 6, 7];
+let arD = [10, 20, 30, 40, 50, 60, 70];
+let copy, index, offset, element;
+let ulSurrounding; 
 
-teudatStrNumber = '345714520';
-console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber), '\n');
+//Task 1
+console.log ("Task 1");
+console.log ("ulSurrounding:", ulSurrounding = ulSurround(strings));
 
-teudatStrNumber = '3457N4523';
-console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber), '\n');
+//Task 2
+console.log ("Task 2");
+element = "abc";
+console.log ("strings:", strings, "element = ", element, "result =", count(strings,element));
 
-teudatStrNumber = '34571452';
-console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber),'\n');
+element = "ab";
+console.log ("strings:", strings, "element = ", element, "result =", count(strings,element));
 
-teudatStrNumber = '324505122';
-console.log ("Teudat Zehut number: ", teudatStrNumber, "Answer: ", checkTeudatZehut(teudatStrNumber),'\n');
+//Task 3
+console.log ("Task 3");
+console.log ("arD:", arrayCopy(numbers, 3, arD, 4, 3));
 
+//Task 4
+console.log ("Task 4");
+copy = numbers.slice(), index = 2, offset = 4;
+console.log ("move:", numbers, "index =", index, "offset =", offset, "result:",  move(copy, index, offset));
 
-console.log ("Create TD:", generateRandomTeudatZehut());
+copy = numbers.slice(), index = 3, offset = -1;
+console.log ("move:", numbers, "index =", index, "offset =", offset, "result:",  move(copy, index, offset));
 
-// first task
-function checkTeudatZehut(teudatStrNumber) 
+// Task 1
+function ulSurround (strings)
 {
-   let arNumberTZ = StringToNumber (teudatStrNumber); //TZ - array of nimbers
-   let check = checkInput (arNumberTZ); //check input of TZ: lenght, symbols
-
-// if input - O.K., check control sum
-  if (check == true)  
-    { 
-      check = false;  
-         let sumTD =  arNumberTZ.reduce (sumTZ,0);
-         if (sumTD % 10 == 0)
-             check = true;
-         else  
-             console.log ("Error3: incorrect TZnumber");
-    }           
-  return check;
+   // add to each element new symbols, new massiv
+   let array = strings.map (function(e) { 
+        return '<li>' + e + '<li>';
+    });
+    // add new elements to the beginning
+    array.splice (0, 0, "<ul>" );
+    // add new elements to the end
+    array.splice (array.length, 0, "<ul>");
+    //.splice return deleted elements
+    return array;
 }
 
-/* V.R. May be following is easier.
-  return Array.from(teudatStrNumber).map(symToNumber);
-  */
-function StringToNumber (teudatStrNumber) //function to arrange string to array of numbers
+//Task 2
+
+function count (strings, element)
 {
-  let arSymbolTZ = Array.from(teudatStrNumber); /*array of symbols: string */
-  let arNumberTZ =  arSymbolTZ.map (symToNumber); /* function get array of numbers */
-     return arNumberTZ;
+   // if element of array == symbol, count++
+   return strings.reduce (function(count, symbol){
+      if (symbol == element) 
+                    count++; 
+     return count;
+  },0);
 }
 
-function symToNumber (symbol, index) //function to arrange symbol to number
-{
-   /* V.R. May be
-  return +symbol;
-  is easier
-  */
-  return Number(symbol);
-} 
+//Task 3
 
-function checkInput (arNumberTZ) //function for checking input
-{
-    let check = true; 
-   if (arNumberTZ.length == 9)
-     {
-      /* !!!!! V.R. This cycle gives true if the LAST number is OK.
-      It isn't correct. It is necessary to fix.
-      */
-     
-      arNumberTZ.forEach(function(symbol)
-      { 
-        if (symbol >= 0 && symbol <= 9)
-            { check = true;}
-        else
-        /* V.R. The function like this hasn't to log.
-        It returns boolean value and caller logs.
-        */
-            {console.log ("Error1: not a number");
-              check = false;  
-            } 
-      });
-     
-     }
-    else {
-      console.log ("Error2: incorrest length");
-        }
-  return check;
+function arrayCopy (src, srcPos, dst, dstPos, length)
+{  // create new array - as a part of source array
+   let array = src.slice(srcPos, srcPos+length);
+   // took element from array (forEach) and put into dst (splice)
+   array.forEach (function(symbol, index) {
+      dst.splice(dstPos+index,0,symbol);})
+   return dst;
 }
 
-function sumTZ (total, symbol, index) //function to calc. control sum
+
+//Task 4
+
+function move (array, index, offset)
 {
-  if (index % 2 == 0)
-     { total += symbol; 
-     }
-  else {
-    let oddN = symbol*2;
-    if (oddN >9)
-      { 
-        total += (oddN % 10) + 1; 
-        // V.R. total += (oddN % 10) + Math.floor(oddN/10);
-      }       
-    else  {
-      total += oddN;
-          }    
-  }
-  return total;  
+
+   let el = array[index];
+   // 2 function splice, because of delete from one place and put into another
+   // delete element from the old place
+   array.splice (index, 1);
+   // put the element for new place
+   array.splice (index+offset, 0, el);
+   //.splice return deleted elements
+   return array;
 }
-
-// second task
-function generateRandomTeudatZehut()
-{
-  // create array from 8 elements
-  let arTZnumber = genArray8();
-  
-  // put control sum to 9-element
-
-  //!!! for case if control sum is multiplied on 10
-  // you get digit 10 
-  //that can not be in the regular decimal computing system
-  //arTZnumber[8] =  10 - (arTZnumber.reduce (sumTZ,0) % 10);
-  // this is new code:
-  let controlNumber = arTZnumber.reduce (sumTZ,0) % 10;
-  controlNumber == 0 ? arTZnumber[8] = controlNumber : arTZnumber[8] = 10-controlNumber;
-   
-       
-  // change array from number to symbol
-  let arSymbolTZ =  arTZnumber.map (numberToSym); 
-  // get string from array of symbols without spasee and comma
-  let newTZ = arSymbolTZ.join('');
-   // check new TZ
-  console.log ("New TD: ", newTZ, "Answer: ", checkTeudatZehut(newTZ),'\n');
-
-  return newTZ;
-} 
-
-function genArray8 () //function to get random array [8]
-{
-  let arNumberTZ8 = [];
-  for (let i=0; i < 8; i++) 
-     {
-      arNumberTZ8[i] = Math.round(Math.random() * 9);
-     }
-   return arNumberTZ8;
-}
-
-function numberToSym (symbol) //function to arrange number to string
-{
-  // V.R. String.fromCharCode(cur+'0'.charCodeAt());
-   return String(symbol);
-} 
-

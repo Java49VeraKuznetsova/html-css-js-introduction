d71e439b8e3540290eb4f9e10104abc6941cad89
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-11-19 17:36:57 +0200

    HW17 ?

--- src/marvels.js
+++ src/marvels.js
@@ -7,119 +7,75 @@ let index;
 let indexPrev=-1;
 let wordFieldAr=[];
 const sectionElement = document.querySelector(".word-guess");
-//!!!
-//sectionElement.innerHTML = getDivsElements();
-const letterElements = document.querySelectorAll(".letter-guess");
+let letterElements 
 const questionElement = document.querySelector(".question");
 const gameOverElement = document.querySelector(".game-over-message");
 const playAgainElement = document.getElementById("play-again");
 let flGameOver = false;
 let colorsOld=[];
 
+
 startGame();
 
 
-function onChange(event) {
-    alert ("start1");
-    //const word = Array.from (words[index][1]);
-    //alert (word);
-   
-   
-
+function onChange(event) 
+{
 const wordGuess = event.target.value;
-event.target.value ='';
-
-checkEnd(flGameOver);
-
-alert (flGameOver);
-
-//alert (wordGuess);
-alert (wordGuess.length);
-alert (wordFieldAr.length);
-if (wordGuess.length != wordFieldAr.length) {
-    alert(`A word should contain ${wordFieldAr.length} letters`)
-} else {
+event.target.value = '';
 const wordAr = Array.from (wordGuess);
-
-
-const colors = wordFieldAr.map((l, i) => {
-  
-   let res;
-   if (colorsOld[i] != 'white')
-     {
-       
-        res =  wordAr.includes(l) ? 'white' : 'black';
-    }
-    else {
-        res = 'white';
-    }
- 
-    
-    return res;
-});
-alert (colors);
+const colors = wordFieldAr.map((l, i) => 
+  {
+    return  colorsOld[i] == 'white' || wordAr.includes(l) ? 'white' : 'black';
+  }
+);
 colorsOld = colors;
-
-//!!!
-colors.forEach((c, i) => letterElements[i].style.background = c);
-
+colors.forEach((c, i) => 
+       letterElements[i].style.background = c);
 if (colors.includes('black') == false)
-{ alert ('end start 1'); endGame(wordAr); }
-else
-{}
+             endGame(wordAr); 
 }
-}
+
 
 function startGame ()
 {
-    alert ("start");
- 
-   alert (indexPrev);
-   gameOverElement.innerHTML = '';
-   //playAgainElement.innerHTML = '';
-   //!!!
+  gameOverElement.innerHTML = '';
+   
+
+   do {
+    index = Math.floor(Math.random() * words.length);
+    console.log ("word" , words[index][1]);
+
+} while (indexPrev == index);
+indexPrev = index;
+
    sectionElement.innerHTML = getDivsElements();
+   letterElements = document.querySelectorAll(".letter-guess");
     questionElement.innerHTML = `Question: ${words[index][0]}`;
-    wordFieldAr.forEach((l, i) => letterElements[i].innerHTML = l);
+    wordFieldAr.forEach((l, i) =>        
+                        letterElements[i].innerHTML = l);
 }
 
 function getDivsElements() {
-    
-    do {
-        index = Math.floor(Math.random() * words.length);
  
-    } while (indexPrev == index);
-    indexPrev = index;
    let wordField = words[index][1];
-    
-    wordFieldAr = Array.from(wordField);
-    let res = wordFieldAr.map(letter => `<div class="letter-guess">${letter}</div>`);
-    return res.join('');
+   wordFieldAr = Array.from(wordField);
+   let res = wordFieldAr.map(letter => `<div class="letter-guess">${letter}</div>`);
+   return res.join('');
 }
 
 function endGame (wordAr) {
-    alert ("start end");
-    alert (wordAr);
-    alert (wordFieldAr);
-    
-     gameOverElement.innerHTML = "All letters now open, if you want to play once more, press the button"
-    
    
-//playAgainElement.innerHTML = "Play again";
-  // playAgainElement.style.display='block';
-   wordGuess.innerHTML = '';
-   
-   flGameOver = true;
-   alert ("indexPrev");
-   //indexPrev = -1;
-}
+    gameOverElement.innerHTML = "All letters now open, if you want to play once more, press the button"
+    gameOverElement.style.color = "green";
+    colorsOld = colorsOld.map (c => c = 'black');
+ }
 
 function checkEnd (flGameOver) {
 if (flGameOver) {
-    alert("game is over");
-    wordGuess.innerHTML = '';
-    gameOverElement.innerHTML = '';
-    //gameOverElement.innerHTML = 'Whaiting you once more';
+   
+   sectionElement.innerHTML = '';
+   questionElement.innerHTML = '';
+   gameOverElement.innerHTML = 'Whaiting you once more';
     return;
 }
 }

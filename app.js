0fa8a1c93df7181220cf5483b2e0c5f7d22b4cb0
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-11-21 23:43:58 +0200

    HW18 Anagram

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 
 const str1 = "yellow";
 const stringsAr = ['weLoly','leloyw','wolley', 'weloyl',
-             'weloll', 'leloy', 'wollet', 'weloyo', 'yelloww', 'YELLOW'];
-
+             'weloll', 'leloy', 'wollet', 'weloyo', 'yelloww', 'YELLOW', 'yelloq'];
 
 
 stringsAr.forEach((e, i) => { isAnagram (str1, stringsAr[i]) == true ? 
@@ -11,51 +10,37 @@ stringsAr.forEach((e, i) => { isAnagram (str1, stringsAr[i]) == true ?
                 });
 
 function isAnagram (str1, str2) {
-let res = true; 
+
 if (str1.lenght != str2.lenght)
-{ return res = false;
+{ return false;
 }
 else {
-    
+   
     const str1ArrayObject = str1Treatment(str1);
-
-const str1Object = {};
-//console.log (str1, str2);
-
-const str1Array = Array.from (str1.toLowerCase());
-
-const str2Array = Array.from (str2.toLowerCase());
-
-
-
-str1Array.forEach (element => {
-
-  
-    str1Object[element] == true ? str1Object[element]++ : str1Object[element]=1;
-
-    });
-
-    const str1ArrayObject = Object.entries(str1Object);
-
+    const str2Array = str2Treatment (str2);
+    let res = true;
 
 str2Array.forEach (el2 => {
               str1ArrayObject.forEach (el1 => {
-           // el2 == el1[0] ? el1--;
-            if (el1[0] == el2) el1[1]-- ;
-            
-          });
+                      if (el1[0] == el2) el1[1]-- ; });
         });
        
-      res = true;
-       str1ArrayObject.forEach (el1 => {
-       // if (el1[1] == 1) res = false;
-       if (el1[1] != 0) res = false;
-       });
-       console.log (res);
-   return res;
+str1ArrayObject.forEach (el1 => {
+        if (el1[1] != 0) res = false;   });
+  return res;
 }
 }
 
 function str1Treatment (str1) {
-    
+    const str1Object = {};
+    const str1Array = Array.from (str1.toLowerCase()); 
+    str1Array.forEach (element => {
+        str1Object[element] == true ? str1Object[element]++ : str1Object[element]=1;
+            });
+    return Object.entries(str1Object);
+}
+
+function str2Treatment (str2) {
+  
+    return Array.from (str2.toLowerCase());
 }

7f318f6bc34129bea16f9caa800d2d18e8b4817d
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-12-05 15:16:09 +0200

    CW 22 - forms

--- src/form.js
+++ src/form.js
@@ -1,54 +1,77 @@
 const inputElements = document.querySelectorAll(".form-class [name]");
-const errorElement = document.querySelector(".error-info");
-
-
-const MIN_YEAR = 1950;
 const MIN_SALARY = 1000;
 const MAX_SALARY = 40000;
-const CURRENT_YEAR = new Date().getFullYear();
+const MIN_YEAR = 1950;
+const maxYear = getMaxYear();
+const TIME_OUT_ERROR_MESSAGE = 5000;
+const ERROR_CLASS = "error";
+const company = new Company();
 
+const dateErrorElement = document.getElementById("date_error");
+const salaryErrorElement = document.getElementById("salary_error");
 
 function onSubmit(event) {
     event.preventDefault();
     console.log("submitted");
-   const employee = Array.from(inputElements).reduce(
+    const employee = Array.from(inputElements).reduce(
         (res, cur) => {
             res[cur.name] = cur.value;
             return res;
         }, {}
     )
     console.log(employee)
+    company.hireEmployee(employee);
+    
 }
 function onChange(event) {
-  let currentEventValue = event.target.value;
-    if (event.target.name == "salary") {
-       /* V.R. The following will do your code more effective
-          const salary = +event.target.value;
-          */
-        if(+event.target.value < MIN_SALARY || +event.target.value > MAX_SALARY){
-          event.target.value='' ;
-          printError(`Error: salary must be from ${MIN_SALARY} to ${MAX_SALARY}`)
-        }
-    }
-    // from Yuri: better else if(event.target.name == "birthDate"){...
-     // V.R. 'else' is redundant. It will be the same without it.
-    else {
-        if (event.target.name == "birthDate") {
-             /* V.R. The following will do your code more effective
-          const year = +event.target.value.substring(0,4);
-          */
-          if(+event.target.value.substring(0,4) < MIN_YEAR ||
-              +event.target.value.substring(0,4) > CURRENT_YEAR){
-                event.target.value='' ;    
-            printError (`Error: birthday must be from ${MIN_YEAR} to ${CURRENT_YEAR}`);  
-             }
-            }
-        } 
-}       
 
-function printError (string) {
-           errorElement.innerHTML = string
-      setTimeout(function() {
-            errorElement.innerHTML = '';
-        },5000) 
-     }
+    if (event.target.name == "salary") {
+        validateSalary(event.target)
+    } else if (event.target.name == "birthDate") {
+        validateBirthdate(event.target);
+    }
+}
+function validateSalary(element) {
+    const value = +element.value;
+    if (value < MIN_SALARY || value > MAX_SALARY) {
+        const message = value < MIN_SALARY ? `salary must be ${MIN_SALARY} or greater`
+            : `salary must be ${MAX_SALARY} or less`;
+        showErrorMessage(element, message, salaryErrorElement);
+    }
+
+}
+function validateBirthdate(element) {
+    const value = +element.value.slice(0, 4);
+    if (value < MIN_YEAR || value > maxYear) {
+        const message = value < MIN_YEAR ? `year must be ${MIN_YEAR} or greater`:
+             `year must be ${maxYear} or less`;
+        showErrorMessage(element, message, dateErrorElement) ;    
+
+    }
+
+}
+function showErrorMessage(element, message, errorElement) {
+    element.classList.add(ERROR_CLASS);
+    errorElement.innerHTML = message;
+    setTimeout(() => {
+        element.classList.remove(ERROR_CLASS);
+        element.value = ''; 
+        errorElement.innerHTML = '';
+    }, TIME_OUT_ERROR_MESSAGE);
+}
+
+function getMaxYear() {
+    return new Date().getFullYear();
+}
+function Company() {
+    this.employees = [];
+}
+Company.prototype.hireEmployee = function(employee) {
+    this.employees.push(employee);
+}
+Company.prototype.getAllEmployees = function(){
+    return this.employees;
+}
+Company.prototype.getEmployeesBySalary = function(salaryFrom, salaryTo) {
+    //TODO
+}

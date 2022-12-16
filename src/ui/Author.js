1debb85468e37ec66279858929c700e373edf768
Vera Kuznetsova <kuznetsova-vera@yandex.ru>
2022-12-16 11:55:58 +0200

    HW 25 separate Author Page Output Author with bind

--- src/ui/Author.js
+++ src/ui/Author.js
@@ -1,25 +1,23 @@
 export class AuthorForm {
     #formElement;
     #authorInputElement;
-    
+
     constructor (params) {
         this.#formElement = document.getElementById(params.idForm);
         this.#authorInputElement = document.getElementById(params.idAuthorInput);
     }
 
 addSubmitHandler (processAuthorFun) {
-  this.#formElement.addEventListener("submit", (event) => {
+  this.#formElement.addEventListener ("submit", 
+  this.handler.bind (this, processAuthorFun))};  
+       
+handler (processAuthorFun, event)  {
     event.preventDefault(); 
     const authorSearch = this.#authorInputElement.value;
-   
     processAuthorFun (authorSearch);
   
-  //  booksAuthorListElement.innerHTML = getBookItems(author);
-
-  }) 
-       
-        
+  }     
     
 }
 
-}
+
